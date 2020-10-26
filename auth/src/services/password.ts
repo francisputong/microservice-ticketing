c92aed7b0ac4cea7c1969a597b7ba76f6079f893
francisputong <francisputong01@gmail.com>
2020-10-26 21:29:59 +0800

    added links in header

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -6,7 +6,7 @@ const scryptAsync = promisify(scrypt);
 export class Password {
   static async toHash(password: string) {
     const salt = randomBytes(8).toString('hex');
-    const buff = (await scryptAsync(password, salt, 5)) as Buffer;
+    const buff = (await scryptAsync(password, salt, 64)) as Buffer;
 
     return `${buff.toString('hex')}.${salt}`;
   }
@@ -14,7 +14,7 @@ export class Password {
   static async compare(storedPassword: string, suppliedPassword: string) {
     const [hashedPassword, salt] = storedPassword.split('.');
 
-    const buff = (await scryptAsync(suppliedPassword, salt, 5)) as Buffer;
+    const buff = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
 
     return buff.toString('hex') === hashedPassword;
   }
