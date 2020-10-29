fa5673ed43adb49618821935a1fbab7fadcfe0dc
francisputong <francisputong01@gmail.com>
2020-10-29 21:18:05 +0800

    changed scrypt to bcrypt

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -1,21 +1,11 @@
-import { scrypt, randomBytes } from 'crypto';
-import { promisify } from 'util';
-
-const scryptAsync = promisify(scrypt);
+import bcrypt from 'bcrypt';
 
 export class Password {
   static async toHash(password: string) {
-    const salt = randomBytes(8).toString('hex');
-    const buff = (await scryptAsync(password, salt, 64)) as Buffer;
-
-    return `${buff.toString('hex')}.${salt}`;
+    return bcrypt.hash(password, 8);
   }
 
   static async compare(storedPassword: string, suppliedPassword: string) {
-    const [hashedPassword, salt] = storedPassword.split('.');
-
-    const buff = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
-
-    return buff.toString('hex') === hashedPassword;
+    return bcrypt.compare(suppliedPassword, storedPassword);
   }
 }
